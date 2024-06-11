8b6d863636fbd409bdc45b5c803a75cdf9f6e3da
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-06-11 17:11:19 +0530

    Add files via upload

---
+++ Lecture 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// Declare three variables and then assign them values 5,8,3 and add them
+	
+	int a=5, b=8, c=3;
+	
+	cout<<a+b+c<<endl<<"a+b+c";
+	
+	return 0;	
+}

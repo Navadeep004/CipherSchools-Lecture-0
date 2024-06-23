165e07f70ff5d72fab37cc779dca9e5424edca04
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-06-23 13:41:34 +0530

    Add files via upload

---
+++ Lecture 9.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	
+	for(i=0;i<10;i++)   // By using the for loop print the numbers from 0 to 9.
+	{
+		cout<<i<<endl;
+	}
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for(i=0;i<=100;i++)   //here by using the for loop print 100 numbers.
+	{
+		cout<<i<<endl;
+	}
+}

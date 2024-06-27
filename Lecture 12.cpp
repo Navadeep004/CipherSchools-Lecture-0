d73b72c94eb60eae362fb1237d1f26a1b3d5ed8e
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-06-27 23:48:18 +0530

    Add files via upload

---
+++ Lecture 12.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int score;
+	
+	do
+	{
+		cin>>score;
+	}
+	while (score<289);
+	
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	while(1)
+	{
+		cout<<"Error";
+	}
+	
+}

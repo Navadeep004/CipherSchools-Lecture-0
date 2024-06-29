585f992a96432b78b62a8832f5e9bbbc74f9c7f7
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-06-30 00:14:02 +0530

    Add files via upload

---
+++ Lecture 20.cpp
@@ -0,0 +1,10 @@
+// for Address of an elements use the ampersand symobl(&).
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	float b;
+	long long c;
+	cout<<"The address of int is "<<&(a)<<endl<<"The address of float is "<<&(b)<<endl<<"the address of long long  is "<<&(c);
+	return 0;
+}

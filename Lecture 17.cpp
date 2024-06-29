42122e112c78aca0800953a8a13495014d85c294
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-06-29 23:18:27 +0530

    Add files via upload

---
+++ Lecture 17.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void printHi()
+{
+	cout<<"Hi"<<endl;
+	return;
+}
+
+int sum(int a,int b)
+{
+	int c;
+	c=a+b;
+	return c;
+}
+
+int main()
+{
+	printHi();
+	int a,b;
+	a = 3;
+	b = 8;
+	int d;
+	d = sum(a,b);
+	cout<<"The sum is equal to: "<<d;
+	return 0;
+}

0847a07328d93a598ce721d96085789f02f8952e
Navadeep004 <119602073+Navadeep004@users.noreply.github.com>
2024-07-12 16:41:25 +0530

    Add files via upload

---
+++ Lecture 34.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+
+vector<int> a;
+cout<<"The size of the vector is : "<<a.size();
+
+int i;
+
+for(i=0;i<5;i++)
+{
+	a.push_back(i+1);
+}
+	cout<<"The size of the vector is : "<<a.size();
+	
+	a.pop_back();
+	cout<<"The size of the vector is : "<<a.size();
+	
+	cout<<"The first element of the vector is : "<<a[0]<<endl;
+	
+	for(auto j=a.begin();j<a.end();j++)
+	{
+		cout<<*j;
+	}
+	cout<<"The capacity of the vector is : "<<a.capacity()<<endl;
+	
+}
+
+
+  
